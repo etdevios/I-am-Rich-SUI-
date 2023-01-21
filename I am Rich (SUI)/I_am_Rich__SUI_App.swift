f1d8df74e87df910818d244edf33d37a64e830ee
Eduard Tokarev <etdevios@gmail.com>
2023-01-21 15:17:14 +0300

    Initial Commit

---
+++ I am Rich (SUI)/I_am_Rich__SUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  I_am_Rich__SUI_App.swift
+//  I am Rich (SUI)
+//
+//  Created by Eduard Tokarev on 21.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct I_am_Rich__SUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
